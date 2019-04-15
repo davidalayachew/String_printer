9c524d27a78c530d5d669e273ed7ee1cad49cced
dalayach <davidalayachew@gmail.com>
2019-04-14 20:34:18 -0400

    Created new class to hold helper functions

--- src/main/java/io/github/dalayach/Print_tools/String_printer.java
+++ src/main/java/io/github/dalayach/Print_tools/String_printer.java
@@ -1,139 +1,20 @@
 package io.github.dalayach.Print_tools;
 
-//TODO   give option to choose border character(s) (ideally, allow there to be a left and right variant, as well as a flip variant)
-//TODO   CHECK LENGTH = it's not exactly clean if somewhere down the line, a String has to go out of bounds to fit
-//TODO   This class is too cluttered - maybe group some of the methods in other classes? How to group?
-//          Maybe helpers in one class? Main printer functions in another? Aliases in a third? Perhaps divide by justification?
+//TODO   Give option to choose border character(s) (ideally, allow there to be a left and right variant, as well as a flip variant)
+//TODO   This class is too cluttered - maybe group some of the methods in other classes? How to group? Maybe helpers in one class?
+//          Main printer functions in another? Aliases in a third? Perhaps divide by justification?
+//
 //TODO   Optimization on some of the for each loops
+//TODO   Maybe make variables like expected_size and borders be instance variables? And build a constructor() and a nextLine() using
+//          those new class variables?
 
-public class String_printer
+public class String_printer extends String_printer_HELPER_FUNCTIONS
 {
 
    public String_printer()
    {}
    
-   public int check_expected_size_is_accurate(int expected_size, String... messages)
-   {
-   
-      int index = messages.length;
-      int max_length = expected_size;
-   
-   
-   //This for loop has room for optimization - specifically...
-   //    - is there some algorithm that would find the highest point fastest? A for loop may not be the most efficient.
-   //    - is it more efficient to have 2 calls to message.length()? Or a single call and save that value to yet another variable? Another way?
-      for(int i = 0; i < messages.length; i++)
-      {
-      
-         if(messages[i].length() > max_length)
-         {
-         
-            index = i;
-            max_length = messages[i].length();
-         
-         }
-      
-         i++;
-      
-      }
-      
-      return index;
-   
-   }
-   
-   public int get_actual_size(int expected_size, String... messages)
-   {
-   
-      int index_of_offending_string = check_expected_size_is_accurate(expected_size, messages);
-      
-      if(index_of_offending_string == messages.length)   //if the expected size was accurate
-      {
-      
-         return expected_size;
-      
-      }
-      
-      else
-      {
-      
-         return messages[index_of_offending_string].length();
-      
-      }
-   
-   }
-   
-   public String specifiers_for_format(int left_buffer, int right_buffer)
-   {
-   
-      return "%" + left_buffer + "s%" + right_buffer + "s";   //centered, but section size may vary amongst each other
-   
-   }
-   
-   public String specifiers_for_format(int buffer, boolean left)
-   {
-   
-      String which_side;   //which side will the string be justified?
-      
-      if(left == true){which_side = "-";} // "-" means the resulting string will be left justified
-      
-      else  {which_side = "";}   // "" means the resulting string will be right justified
-   
-      return "%" + which_side + buffer + "s";
-   
-   
-   }
-   
-   public String create_formatted_String(int left_buffer, int right_buffer, String message)
-   {
-   
-      int expected_size = left_buffer + right_buffer;
-   
-      if(left_buffer == 0)
-      {
-         return String.format(specifiers_for_format(expected_size, true), message);}
-      
-      else if(right_buffer == 0)
-      {
-         return String.format(specifiers_for_format(expected_size, false), message);}
-      
-      else
-      {
-      
-         return String.format(specifiers_for_format(left_buffer, right_buffer), message, " ");   //centered, but section size may vary amongst each other
-      
-      }
-   
-   }
-   
-   public int get_buffer_size(int expected_size, int message_length)
-   {
-   
-      return ((expected_size - message_length)/2) + ((expected_size - message_length)%2); 
-      //since dividing by 2 might give us a remainder of one (it will when dividing an odd number), 
-      //we have elected to add the extra space to the right_buffer
-   
-   }
-   
-   public String give_borders(boolean borders)
-   {
-   
-      if(borders == true)
-      {
-      
-         return "|";
-      
-      }
-      
-      else
-      {
-      
-         return "";
-      
-      }
-      
-   
-   }
-   
+
    public String print_center_justified(int expected_size, String... messages)
    {
    
@@ -289,32 +170,36 @@ public class String_printer
       String result =  print_center_justified(borders, total, messages);  System.out.println();  
       return result + "\n";}
    
-   public String print_l(int total, String... messages)  {  
+   public String print_l(int total, String... messages)
+   {
       return print_left_justified(total, messages);  }
    
-   public String print_l(boolean borders, int total, String... messages)  {  
+   public String print_l(boolean borders, int total, String... messages)
+   {  
       return print_left_justified(borders, total, messages);  }
    
-   public String println_l(int total, String... messages)  {  
-      String result =  print_left_justified(total, messages);  System.out.println();  
+   public String println_l(int total, String... messages)
+   {String result =  print_left_justified(total, messages);  System.out.println();  
       return result + "\n";}
    
-   public String println_l(boolean borders, int total, String... messages)  {  
-      String result =  print_left_justified(borders, total, messages);  System.out.println();  
+   public String println_l(boolean borders, int total, String... messages)  
+   {  String result =  print_left_justified(borders, total, messages);  System.out.println();  
       return result + "\n";}
    
-   public String print_r(int total, String... messages)  {  
+   public String print_r(int total, String... messages)
+   {  
       return print_right_justified(total, messages);  }
    
-   public String print_r(boolean borders, int total, String... messages)  {  
+   public String print_r(boolean borders, int total, String... messages)  
+   {  
       return print_right_justified(borders, total, messages);  }
    
-   public String println_r(int total, String... messages)  {  
-      String result =  print_right_justified(total, messages);  System.out.println();  
+   public String println_r(int total, String... messages)  
+   {  String result =  print_right_justified(total, messages);  System.out.println();  
       return result + "\n";}
    
-   public String println_r(boolean borders, int total, String... messages)  {  
-      String result =  print_right_justified(borders, total, messages);  System.out.println();  
+   public String println_r(boolean borders, int total, String... messages)
+   { String result =  print_right_justified(borders, total, messages);  System.out.println();  
       return result + "\n";}
       
    public String create_formatted_string(int left_buffer, int right_buffer, String message)
---
+++ src/main/java/io/github/dalayach/Print_tools/String_printer_HELPER_FUNCTIONS.java
@@ -0,0 +1,129 @@
+package io.github.dalayach.Print_tools;
+
+class String_printer_HELPER_FUNCTIONS
+{
+
+   public int check_expected_size_is_accurate(int expected_size, String... messages)
+   {
+   
+      int index = messages.length;
+      int max_length = expected_size;
+   
+   
+   //This for loop has room for optimization - specifically...
+   //    - is there some algorithm that would find the highest point fastest? A for loop may not be the most efficient.
+   //    - is it more efficient to have 2 calls to message.length()? Or a single call and save that value to yet another variable? Another way?
+      for(int i = 0; i < messages.length; i++)
+      {
+      
+         if(messages[i].length() > max_length)
+         {
+         
+            index = i;
+            max_length = messages[i].length();
+         
+         }
+      
+         i++;
+      
+      }
+      
+      return index;
+   
+   }
+   
+   public int get_actual_size(int expected_size, String... messages)
+   {
+   
+      int index_of_offending_string = check_expected_size_is_accurate(expected_size, messages);
+      
+      if(index_of_offending_string == messages.length)   //if the expected size was accurate
+      {
+      
+         return expected_size;
+      
+      }
+      
+      else
+      {
+      
+         return messages[index_of_offending_string].length();
+      
+      }
+   
+   }
+   
+   public String specifiers_for_format(int left_buffer, int right_buffer)
+   {
+   
+      return "%" + left_buffer + "s%" + right_buffer + "s";   //centered, but section size may vary amongst each other
+   
+   }
+   
+   public String specifiers_for_format(int buffer, boolean left)
+   {
+   
+      String which_side;   //which side will the string be justified?
+      
+      if(left == true){which_side = "-";} // "-" means the resulting string will be left justified
+      
+      else  {which_side = "";}   // "" means the resulting string will be right justified
+   
+      return "%" + which_side + buffer + "s";
+   
+   
+   }
+   
+   public String create_formatted_String(int left_buffer, int right_buffer, String message)
+   {
+   
+      int expected_size = left_buffer + right_buffer;
+   
+      if(left_buffer == 0)
+      {
+         return String.format(specifiers_for_format(expected_size, true), message);}
+      
+      else if(right_buffer == 0)
+      {
+         return String.format(specifiers_for_format(expected_size, false), message);}
+      
+      else
+      {
+      
+         return String.format(specifiers_for_format(left_buffer, right_buffer), message, " ");   //centered, but section size may vary amongst each other
+      
+      }
+   
+   }
+   
+   public int get_buffer_size(int expected_size, int message_length)
+   {
+   
+      return ((expected_size - message_length)/2) + ((expected_size - message_length)%2); 
+      //since dividing by 2 might give us a remainder of one (it will when dividing an odd number), 
+      //we have elected to add the extra space to the right_buffer
+   
+   }
+   
+   public String give_borders(boolean borders)
+   {
+   
+      if(borders == true)
+      {
+      
+         return "|";
+      
+      }
+      
+      else
+      {
+      
+         return "";
+      
+      }
+      
+   
+   }
+   
+
+}
