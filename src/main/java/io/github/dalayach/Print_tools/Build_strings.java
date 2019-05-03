6829ef6d2da40e8b3b1bc98b3bc8509e09f91bdc
dalayach <davidalayachew@gmail.com>
2019-05-03 19:10:01 -0400

    Added several new aliases for newline

--- src/main/java/io/github/dalayach/Print_tools/Build_strings.java
+++ src/main/java/io/github/dalayach/Print_tools/Build_strings.java
@@ -209,4 +209,109 @@ class Build_strings extends Helper_functions
    
    }
 
+   /* NEW LINE ALIASES */
+
+   /**
+    * Will print and return a sequence of Strings that is center-justified. Each will be separated with borders
+    * if the boolean parameter <code>borders</code> is true.
+    * 
+    * @param   borders        boolean that is true if borders are to be included, and false if not. Note that borders
+    *                            will add to the column width, so a sequence of Strings WITH borders will be longer
+    *                            than a sequence of Strings WITHOUT borders.
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now center-justified
+    */
+   public String println_center_justified(boolean borders, int expected_size, String... messages)
+   {
+   
+      return print_center_justified(borders, expected_size, messages).concat("\n");
+   }
+   
+
+   /**
+    * Will print and return a sequemce of Strings that is center-justified and separated with borders.
+    * 
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now center-justified with borders
+    */
+   public String println_center_justified(int expected_size, String... messages)
+   {
+      
+      return println_center_justified(true, expected_size, messages);
+   
+   }
+   
+
+   /**
+    * Will print and return a sequence of Strings that is left-justified. Each will be separated with borders
+    * if the boolean parameter <code>borders</code> is true.
+    * 
+    * @param   borders        boolean that is true if borders are to be included, and false if not. Note that borders
+    *                            will add to the column width, so a sequence of Strings WITH borders will be longer
+    *                            than a sequence of Strings WITHOUT borders.
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now left-justified
+    */
+   public String println_left_justified(boolean borders, int expected_size, String... messages)
+   {
+         
+      return print_left_justified(borders, expected_size, messages).concat("\n");
+         
+   }
+   
+   /**
+    * Will print and return a sequemce of Strings that is left-justified and separated with borders.
+    * 
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now left-justified with borders
+    */
+   public String println_left_justified(int expected_size, String... messages)
+   {
+   
+      return println_left_justified(true, expected_size, messages);
+   
+   }
+
+   /**
+    * Will print and return a sequence of Strings that is right-justified. Each will be separated with borders
+    * if the boolean parameter <code>borders</code> is true.
+    * 
+    * @param   borders        boolean that is true if borders are to be included, and false if not. Note that borders
+    *                            will add to the column width, so a sequence of Strings WITH borders will be longer
+    *                            than a sequence of Strings WITHOUT borders.
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now right-justified
+    */
+   public String println_right_justified(boolean borders, int expected_size, String... messages)
+   {
+         
+      return print_right_justified(borders, expected_size, messages).concat("\n");
+   
+   }
+   
+   /**
+    * Will print and return a sequemce of Strings that is right-justified and separated with borders.
+    * 
+    * @param   expected_size  int containing the expected_size (given by the user, but we will check if it's correct)
+    * @param   messages       sequence of Strings (or just a String array, depending how you wish to use it) that holds
+    *                            the data to be printed out as a String
+    * @return                 the newly created String that is now right-justified with borders
+    */
+   public String println_right_justified(int expected_size, String... messages)
+   {
+         
+      return println_right_justified(true, expected_size, messages);
+   
+   
+   }
 }
